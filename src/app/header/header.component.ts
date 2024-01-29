eb6729136a807b63e0712e5f6f5aa74ce93108ce
Saksham Gupta <saksham757gupta@gmail.com>
2024-01-29 17:16:35 +0530

    updated seller page for menu items

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private router: Router) {}
+  menuType: string = 'default';
 
+  ngOnInit(): void {
+    this.router.events.subscribe((value: any) => {
+      if (value.url) {
+        if (localStorage.getItem('seller') && value.url.includes('seller')) {
+          this.menuType = 'seller';
+        } else {
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
 }
